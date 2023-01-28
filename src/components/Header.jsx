9ade25e5875d7388d9cd1ed040f490272126dcbb
Victor Seleimend <vsennaselei@gmail.com>
2023-01-28 16:35:13 -0300

    Mudança no texto do título

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => (
   <h1 className="font-black text-4xl md:text-5xl text-center md:w-2/3 mx-auto">
-    Seguimento de Pacientes <span className="text-indigo-600">Veterinária</span>
+    Seguimento de Pacientes <span className="text-indigo-600">Vet</span>
   </h1>
 );
 
